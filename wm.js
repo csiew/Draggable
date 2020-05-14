0f2802a94c73a4c77546b5fdef9737dd0655a438
csiew <clarence.siew@gmail.com>
2020-05-14 21:08:53 +1000

    Separate initialisation in startup script

--- wm.js
+++ wm.js
@@ -732,33 +732,3 @@ class Preferences {
         currentSession.createWindow(prefsBackground);
     }
 }
-
-class SessionManager {
-    constructor() {
-        this.windowReg = new Map();
-    }
-}
-
-currentSession = new wmSession();
-currentSession.init();
-taskmgr = new TaskManager();
-pool = new PoolManager();
-prefs = new Preferences();
-pool.batchAdd([
-    [
-        "Burgundy",
-        "That might make a good name for this."
-    ],
-    [
-        "Editor",
-        `
-            <button>Open</button>
-            <button>Save</button>
-            <button>Save As...</button>
-        `
-    ],
-    [
-        "Video",
-        '<iframe width="560" height="315" src="https://www.youtube.com/embed/lWM2kqdP1bc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-    ]
-]);
